ceaf43d8adea7e5b2a1924607dd239a418fe8920
paulakora <100996895+paulakora@users.noreply.github.com>
2025-04-26 18:37:28 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,38 @@
-from openai import OpenAI
+import streamlit as st
+import random
+import time
 
-client = OpenAI(
-  base_url="https://openrouter.ai/api/v1",
-  api_key="<API_KEY>",
-)
+st.write("Streamlit loves LLMs! 🤖 [Build your own chat app](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps) in minutes, then make it powerful by adding images, dataframes, or even input widgets to the chat.")
+
+st.caption("Note that this demo app isn't actually connected to any LLMs. Those are expensive ;)")
+
+if "messages" not in st.session_state:
+    st.session_state.messages = [{"role": "assistant", "content": "Let's start chatting! 👇"}]
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("What is up?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    assistant_response = random.choice([
+        "Hello there! How can I assist you today?",
+        "Hi, human! Is there anything I can help you with?",
+        "Do you need help?",
+    ])
+
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+        for chunk in assistant_response.split():
+            full_response += chunk + " "
+            message_placeholder.markdown(full_response + "▌")  
+            time.sleep(0.05)
+        message_placeholder.markdown(full_response.strip())
+
+    st.session_state.messages.append({"role": "assistant", "content": full_response.strip()})
+
